f754c4b286260a376332e9c2ed42a0cd1b8cbe78
Aozsah <ahmet.ozkan.sahin@outlook.com>
2022-11-10 02:51:39 +0300

    API's update

--- product/views.py
+++ product/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Product
-from .serializer import ProductSerializer
-
+from .models import Product, Category
+from .serializer import ProductSerializer, CategorySerializer
 
 class LatestProductList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404    
+    
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):
+     def get_object(self, category_slug,):
+        try:
+            return Category.objects.filter.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404    
+        
+     def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
